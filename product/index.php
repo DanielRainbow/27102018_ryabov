3d7cc2ff75630d4b0afb44c4c6e89000503415bd
DanielRainbow <Danila436@mail.ru>
2019-03-28 17:19:30 +0300

    Добавление корзины

--- product/index.php
+++ product/index.php
@@ -43,9 +43,6 @@
 
 <script src="/js/product.js"></script>
 
-<button class='add-to-basket' data-product-id='<?=$template['product']['id']?>'>Добавить в корзину</button>
-
-<script src="/js/product.js"></script>
 <?php 
     include($_SERVER['DOCUMENT_ROOT'].'/parts/footer.php');
 ?> 
